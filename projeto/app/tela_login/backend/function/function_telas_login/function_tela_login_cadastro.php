fdae3727cbf4c7726570797ef9e7d99796aa065c
devlongen <iago.mendonca_2005@hotmail.com>
2024-05-15 13:13:33 -0300

    tela de login funcionando perfeitamente

--- projeto/app/tela_login/backend/function/function_telas_login/function_tela_login_cadastro.php
+++ projeto/app/tela_login/backend/function/function_telas_login/function_tela_login_cadastro.php
@@ -10,9 +10,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cadastro_db"])) {
     $cpf_cadastro_db = $_POST['cpf_insert_db'];
     $telefone_cadastro_db = $_POST['telefone_insert_db'];
     $data_cadastro_db = $_POST['data_de_nascimento_insert_db'];
-    $tipo_usuario = $_POST['tipo_usuario_db'];
+    $tipo_usuario_db = $_POST['tipo_usuario_db'];
     # Chama a função de inserção no banco de dados
-    insert_db($conexao, $nome_cadastro_db, $email_cadastro_db, $senha_cadastro_db, $cpf_cadastro_db, $telefone_cadastro_db, $data_cadastro_db, $tipo_usuario);
+    insert_db($conexao, $nome_cadastro_db, $email_cadastro_db, $senha_cadastro_db, $cpf_cadastro_db, $telefone_cadastro_db, $data_cadastro_db, $tipo_usuario_db);
 }
 # Verifica se o formulário de login foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login_db"])) {
@@ -25,21 +25,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login_db"])) {
 }
 
 # Função para inserir dados no banco de dados
-function insert_db($conexao, $nome_cadastro_db, $email_cadastro_db, $senha_cadastro_db, $cpf_cadastro_db, $telefone_cadastro_db, $data_cadastro_db, $tipo_usuario)
+function insert_db($conexao, $nome_cadastro_db, $email_cadastro_db, $senha_cadastro_db, $cpf_cadastro_db, $telefone_cadastro_db, $data_cadastro_db, $tipo_usuario_db)
 {
     # Criptografa a senha
     $senha_hash = password_hash($senha_cadastro_db, PASSWORD_DEFAULT);
     # Prepara a declaração SQL para inserir os dados
     $stmt = $conexao->prepare("INSERT INTO usuario (nome_usuario, email_usuario, senha_usuario, cpf_usuario, telefone_usuario, data_nascimento_usuario, status_usuario) VALUES (?, ?, ?, ?, ?, ?, ?)");
     # Vincula os parâmetros à declaração SQL
-    $stmt->bind_param("sssssss", $nome_cadastro_db, $email_cadastro_db, $senha_hash, $cpf_cadastro_db, $telefone_cadastro_db, $data_cadastro_db, $tipo_usuario);
+    $stmt->bind_param("sssssss", $nome_cadastro_db, $email_cadastro_db, $senha_hash, $cpf_cadastro_db, $telefone_cadastro_db, $data_cadastro_db, $tipo_usuario_db);
     # Executa a declaração SQL
     $stmt->execute();
     session_start();
     $_SESSION['usuario'] = array(
         'nome' => $nome_cadastro_db,
         'email' => $email_cadastro_db,
-        'usuario' => $tipo_usuario
+        'usuario' => $tipo_usuario_db
     );
     header("Location: ../../../../");
 }
@@ -51,7 +51,7 @@ function verify_login_db($conexao, $email_login_db, $senha_login_db)
     session_start();
 
     // Prepara a consulta SQL para selecionar o usuário pelo email
-    $stmt = $conexao->prepare("SELECT id_usuario, email_usuario, senha_usuario, status_usuario FROM usuario WHERE email_usuario = ?");
+    $stmt = $conexao->prepare("SELECT idusuario, email_usuario, senha_usuario, status_usuario FROM usuario WHERE email_usuario = ?");
     // Verifica se a preparação da consulta falhou
     if ($stmt === false) {
         echo "Erro ao preparar a consulta SQL: " . $conexao->error;
@@ -63,7 +63,7 @@ function verify_login_db($conexao, $email_login_db, $senha_login_db)
     // Executa a consulta SQL
     if ($stmt->execute()) {
         // Vincula o resultado da consulta a variáveis
-        $stmt->bind_result($id_usuario, $email_usuario, $senha_usuario, $tipo_usuario);
+        $stmt->bind_result($id_usuario, $email_usuario, $senha_usuario, $tipo_usuario_db);
         // Obtém o resultado da consulta
         $stmt->fetch();
         // Verifica se o usuário foi encontrado
@@ -73,7 +73,7 @@ function verify_login_db($conexao, $email_login_db, $senha_login_db)
                 // Se a senha estiver correta, define a sessão do usuário
                 $_SESSION['user_id'] = $id_usuario;
                 $_SESSION['email'] = $email_usuario;
-                $_SESSION['usuario'] = $tipo_usuario;
+                $_SESSION['usuario'] = $tipo_usuario_db;
 
                 // Redireciona o usuário para a página de perfil, por exemplo
                 header("Location: ../../../../");
