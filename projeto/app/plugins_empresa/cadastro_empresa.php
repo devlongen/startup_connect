fd598863f7152a58061ee2ae157ff91aac4675b8
ElyandeAndrade <elyan.andrade3@gmail.com>
2024-05-16 20:41:45 -0300

    up

--- projeto/app/plugins_empresa/cadastro_empresa.php
+++ projeto/app/plugins_empresa/cadastro_empresa.php
@@ -5,19 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="../../plugins/summernote/summernote-bs4.min.css">
-  <!-- CodeMirror -->
-  <link rel="stylesheet" href="../../plugins/codemirror/codemirror.css">
-  <link rel="stylesheet" href="../../plugins/codemirror/theme/monokai.css">
-  <!-- SimpleMDE -->
-  <link rel="stylesheet" href="../../plugins/simplemde/simplemde.min.css">
+    <link rel="stylesheet" href="assets/css/style.css"> 
+    <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css"> 
+    <!-- <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css"> -->
+  
     
 
     
@@ -89,25 +80,24 @@
                     </div>
 
 
-                    <div class="row">
-        <div class="col-md-12">
-          <div class="card card-outline card-info">
-            <div class="card-header">
-              <h3 class="card-title">
-                Summernote
-              </h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <textarea id="summernote">
-                Place <em>some</em> <u>text</u> <strong>here</strong>
-              </textarea>
-            </div>
-            <div class="card-footer">
-              Visit <a href="https://github.com/summernote/summernote/">Summernote</a> documentation for more examples and information about the plugin.
-            </div>
-          </div>
-        </div>
+                    <div class="col-md-12">
+                            <div class="card card-outline card-info">
+                                <div class="card-header">
+                                <h3 class="card-title">
+                                    Summernote
+                                </h3>
+                                </div>
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                <textarea id="summernote">
+                                    Place <em>some</em> <u>text</u> <strong>here</strong>
+                                </textarea>
+                                </div>
+                                <div class="card-footer">
+                                    Visit <a href="https://github.com/summernote/summernote/">Summernote</a> documentation for more examples and information about the plugin.
+                                </div>
+                            </div>
+                    </div>
               
 
                 </div>
@@ -148,35 +138,28 @@
             </form>
         </div>
     </div>
+    
 
-    <script src="../plugins/summernote/summernote-bs4.min.js"></script>
     <!-- jQuery -->
-    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <script src="../plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../../dist/js/adminlte.min.js"></script>
-    <!-- CodeMirror -->
-    <script src="../../plugins/codemirror/codemirror.js"></script>
-    <script src="../../plugins/codemirror/mode/css/css.js"></script>
-    <script src="../../plugins/codemirror/mode/xml/xml.js"></script>
-    <script src="../../plugins/codemirror/mode/htmlmixed/htmlmixed.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="../../dist/js/demo.js"></script>
-    <!-- Page specific script -->
+    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Summernote -->
+    <script src="../plugins/summernote/summernote-bs4.min.js"></script>
+    
 
     <script>
-                             $(function () {
-                                // Summernote
-                                $('#summernote').summernote()
+  $(function () {
+    // Summernote
+    $('#summernote').summernote()
 
-                                // CodeMirror
-                                CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
-                                mode: "htmlmixed",
-                                theme: "monokai"
-                                });
-                            })
-        </script>
+    // CodeMirror
+    CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
+      mode: "htmlmixed",
+      theme: "monokai"
+    });
+  })
+</script>
 
 
 
