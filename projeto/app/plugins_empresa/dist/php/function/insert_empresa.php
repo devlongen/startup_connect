d34c54265c93f13db9963e8933985463fbf597b0
devlongen <iago.mendonca_2005@hotmail.com>
2024-05-19 22:04:24 -0300

    começando a criação de startup pro banco de dados

--- projeto/app/plugins_empresa/dist/php/function/insert_empresa.php
+++ projeto/app/plugins_empresa/dist/php/function/insert_empresa.php
@@ -2,7 +2,17 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cadastro_empresa"])) {
     # Inclui o arquivo de conexão com o banco de dados
-    include_once('../../../conn_db.php');
+    include_once('../../../../conn_db.php');
+
+    $razao = $_POST[''];
+    $cnpj = $_POST[''];
+    $nome_fantasia = $_POST[''];
+    $endereco = $_POST[''];
+    $email_corporativo = $_POST[''];
+    $meta_total = $_POST[''];
+
+    
+
 
     
 }
