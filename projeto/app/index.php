aeaab5d83f3902abafacd99b6d2cc66d62ef0e41
ElyandeAndrade <elyan.andrade3@gmail.com>
2024-05-20 20:42:37 -0300

    up

--- projeto/app/index.php
+++ projeto/app/index.php
@@ -1,5 +1,5 @@
 <?php session_start();
-    include_once('dist/php/controle_sessao.php')
+    include_once('dist/php/controle_sessao.php');
 ?>
 
 <!DOCTYPE html>
--- projeto/app/plugins_empresa/cadastro_empresa.php
+++ projeto/app/plugins_empresa/cadastro_empresa.php
@@ -1,154 +1,114 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css"> 
-    <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css"> 
-    <!-- <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css"> -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   
-    
+  <title>Cadastro da Empresa</title>
 
-    
-    <title>Formulário</title>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Summernote -->
+  <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css">
+  
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
-
 <body>
-    <div class="container">
-        <!-- <div class="form-image">
-            <img src="assets/img//undraw_shopping_re_3wst.svg" alt="">
-        </div> -->
-        <div class="card-body">
-              
+
+<div class="container">
+  <div class="form">
+    <form action="#">
+      <div class="form-header">
+        <div class="title">
+          <h1>Cadastro da empresa</h1>
+        </div>
+      </div>
+
+      <div class="input-group">
+        <div class="input-box">
+          <label for="razao-social">Razão Social</label>
+          <input id="razao-social" type="text" name="razao-social" placeholder="Digite a razão social" required>
         </div>
 
-        <div class="form">
-            <form action="dist/php/function/insert_empresa.php" method="POST">
-                <div class="form-header">
-                    <div class="title">
-                        <h1>Cadastro da empresa</h1>
-                    </div>
-
-                </div>
-
-                <div class="input-group">
-                    <div class="input-box">
-                        <label for="firstname">Razão Social</label>
-                        <input id="firstname" type="text" name="firstname" placeholder="Digite seu primeiro nome" required>
-                    </div>
-
-
-                    <div class="input-box">
-                        <label for="lastname">CNPJ</label>
-                        <input id="lastname" type="text" name="lastname" placeholder="Digite seu sobrenome" required>
-                    </div>
-                    <div class="input-box">
-                        <label for="email">Email Corporativo</label>
-                        <input id="email" type="email" name="email" placeholder="Digite seu e-mail" required>
-                    </div>
-
-                    <div class="input-box">
-                        <label for="number">Nome Fantasia</label>
-                        <input id="number" type="tel" name="number" placeholder="(xx) xxxx-xxxx" required>
-                    </div>
-
-                    <div class="input-box">
-                        <label for="password">Endereço</label>
-                        <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
-                    </div>
-
-                    <div class="input-box">
-                        <label for="password">Data de abertura da empresa</label>
-                        <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
-                    </div>
-
-                    <div class="input-box">
-                        <label for="password">Data de abertura da empresa</label>
-                        <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
-                    </div>
-
-                    <div class="input-box">
-                        <label for="password">Patrimônio Oferecido</label>
-                        <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
-                    </div>
-
-                    <div class="input-box">
-                        <label for="password">Meta total</label>
-                        <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
-                    </div>
-
-
-                    <div class="col-md-12">
-                            <div class="card card-outline card-info">
-                                <div class="card-header">
-                                <h3 class="card-title">
-                                    Summernote
-                                </h3>
-                                </div>
-                                <!-- /.card-header -->
-                                <div class="card-body">
-                                <textarea id="summernote">
-                                    Place <em>some</em> <u>text</u> <strong>here</strong>
-                                </textarea>
-                                </div>
-                                <div class="card-footer">
-                                    Visit <a href="https://github.com/summernote/summernote/">Summernote</a> documentation for more examples and information about the plugin.
-                                </div>
-                            </div>
-                    </div>
-              
-
-                </div>
-
-                <div class="gender-inputs">
-                    <div class="gender-title">
-                        <h6>Gênero</h6>
-                    </div>
-
-                    <div class="gender-group">
-                        <div class="gender-input">
-                            <input id="female" type="radio" name="gender">
-                            <label for="female">Feminino</label>
-                        </div>
-
-                        <div class="gender-input">
-                            <input id="male" type="radio" name="gender">
-                            <label for="male">Masculino</label>
-                        </div>
-
-                        <div class="gender-input">
-                            <input id="others" type="radio" name="gender">
-                            <label for="others">Outros</label>
-                        </div>
-
-                        <div class="gender-input">
-                            <input id="none" type="radio" name="gender">
-                            <label for="none">Prefiro não dizer</label>
-                        </div>
-                    </div>
-                </div>
-
-              
-
-                <div class="continue-button">
-                    <button><a href="#" name="cadastro_empresa">Continuar</a> </button>
-                </div>
-            </form>
+        <div class="input-box">
+          <label for="cnpj">CNPJ</label>
+          <input id="cnpj" type="text" name="cnpj" placeholder="Digite o CNPJ" required>
         </div>
-    </div>
-    
 
-    <!-- jQuery -->
-    <script src="../plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- Summernote -->
-    <script src="../plugins/summernote/summernote-bs4.min.js"></script>
-    
+        <div class="input-box">
+          <label for="email">Email Corporativo</label>
+          <input id="email" type="email" name="email" placeholder="Digite seu e-mail corporativo" required>
+        </div>
 
-    <script>
+        <div class="input-box">
+          <label for="nome-fantasia">Nome Fantasia</label>
+          <input id="nome-fantasia" type="text" name="nome-fantasia" placeholder="Digite o nome fantasia" required>
+        </div>
+
+        <div class="input-box">
+          <label for="endereco">Endereço</label>
+          <input id="endereco" type="text" name="endereco" placeholder="Digite o endereço" required>
+        </div>
+
+        <div class="input-box">
+          <label for="data-abertura">Data de abertura da empresa</label>
+          <input id="data-abertura" type="date" name="data-abertura" required>
+        </div>
+
+        <div class="input-box">
+          <label for="patrimonio">Patrimônio Oferecido</label>
+          <input id="patrimonio" type="text" name="patrimonio" placeholder="Digite o patrimônio oferecido" required>
+        </div>
+
+        <div class="input-box">
+          <label for="meta-total">Meta total</label>
+          <input id="meta-total" type="text" name="meta-total" placeholder="Digite a meta total" required>
+        </div>
+
+        <div class="col-md-12">
+          <div class="card card-outline card-info">
+            <div class="card-header">
+              <h3 class="card-title">Summernote</h3>
+            </div>
+            <div class="card-body">
+              <textarea id="summernote">Place <em>some</em> <u>text</u> <strong>here</strong></textarea>
+            </div>
+            
+          </div>
+        </div>
+
+  
+
+      </div>
+
+      <div class="continue-button">
+        <button type="submit">Continuar</button>
+      </div>
+    </form>
+  </div>
+</div>
+
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- Summernote -->
+<script src="../plugins/summernote/summernote-bs4.min.js"></script>
+<!-- CodeMirror -->
+<script src="../plugins/codemirror/codemirror.js"></script>
+<script src="../plugins/codemirror/mode/css/css.js"></script>
+<script src="../plugins/codemirror/mode/xml/xml.js"></script>
+<script src="../plugins/codemirror/mode/htmlmixed/htmlmixed.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- Page specific script -->
+<script>
   $(function () {
     // Summernote
     $('#summernote').summernote()
@@ -160,9 +120,5 @@
     });
   })
 </script>
-
-
-
 </body>
-
 </html>
