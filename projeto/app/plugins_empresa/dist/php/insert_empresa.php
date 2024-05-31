4d075aa9549aa070a72079e98f3b75aaed3d764d
devlongen <iago.mendonca_2005@hotmail.com>
2024-05-31 19:29:48 -0300

    Add comentário do problema

--- projeto/app/plugins_empresa/dist/php/insert_empresa.php
+++ projeto/app/plugins_empresa/dist/php/insert_empresa.php
@@ -1,8 +1,6 @@
 <?php 
-
-// PROBLEMA QUE NÃO ESTÁ ACESSANDO ESSE ARQUIVO
-
 session_start();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Inclui o arquivo de conexão com o banco de dados
     include_once('conexao_empresa.php');
@@ -17,20 +15,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $meta_total = $_POST['meta-total'];
     $descricao = $_POST['desc-startup'];
     $termo_condicao = $_POST['termos-condicao'];
-    // Verifica se a sessão do usuário está iniciada
-    //select_idusuario($conexao);
+    
+    select_idusuario($conexao);
+    insert_logprojeto($conexao, $cnpj, $nome_fantasia);
     insert_termo($conexao, $termo_condicao);
-    //insert_logprojeto($conexao, $descricao, $nome_fantasia);
-    //insert_empresa($conexao, $razao_social, $cnpj, $email, $nome_fantasia, $endereco, $data_abertura, $patrimonio, $meta_total, $descricao, $termo_condicao, $fk_idusuario, $fk_idlog_projeto);
+    insert_empresa($conexao, $razao_social, $cnpj, $email, $nome_fantasia, $endereco, $data_abertura, $patrimonio, $meta_total, $descricao, $termo_condicao, $fk_idusuario, $fk_idlog_projeto);
 }
-
+function select_idusuario($conexao) {
+    $stmt_idusuario = $conexao->prepare("SELECT idusuario FROM usuario;");
+    $stmt_idusuario->bind_param("s", $idusuario);
+    $stmt_idusuario->execute();
+    $stmt_idusuario->close();
+    return $idusuario;
+}
 function insert_empresa($conexao, $razao_social, $cnpj, $email, $nome_fantasia, $endereco, $data_abertura, $patrimonio, $meta_total, $descricao, $termo_condicao, $fk_idusuario, $fk_idlog_projeto) {
-    // Preparar a declaração SQL com placeholders
-    $stmt_empresa = $conexao->prepare("INSERT INTO projeto (razao_social, cnpj_projeto, nome_fantasia, endereco, email_corporativo, data_abertura_empresa, data_abertura_site, patrimonio_oferecido, meta_total, desc_empresa, fk_idtermo_condicao, fk_idusuario, fk_idlog_projeto) 
-                               VALUES (?, ?, ?, ?, ?, ?, CURRENT_DATE(), ?, ?, ?, ?, ?, ?)");
-    $stmt_empresa->bind_param("ssssssssssss", $razao_social, $cnpj, $nome_fantasia, $endereco, $email, $data_abertura, $patrimonio, $meta_total, $descricao, $termo_condicao, $fk_idusuario, $fk_idlog_projeto);
+    $stmt_empresa = $conexao->prepare("INSERT INTO projeto (razao_social, cnpj_projeto, nome_fantasia, endereco, email_corporativo, data_abertura_empresa, data_abertura_site, patrimonio_oferecido, meta_total, desc_empresa, fk_idtermo_condicao, fk_idusuario, fk_idlog_projeto) VALUES (?, ?, ?, ?, ?, ?, CURRENT_DATE(), ?, ?, ?, ?, ?, ?)");
+    $stmt_empresa->bind_param("sssssssssssss", $razao_social, $cnpj, $nome_fantasia, $endereco, $email, $data_abertura, $patrimonio, $meta_total, $descricao, $termo_condicao, $fk_idusuario, $fk_idlog_projeto);
     $stmt_empresa->execute();
-    // Fechar a declaração preparada
     $stmt_empresa->close();
 }
 
@@ -39,29 +40,14 @@ function insert_termo($conexao, $termo_condicao) {
     $stmt_termo->bind_param("s", $termo_condicao);
     $stmt_termo->execute();
     $stmt_termo->close();
-    echo $stmt_termo;
 }
 
-function insert_logprojeto($conexao, $descricao, $nome_fantasia) {
-    $stmt_logprojeto = $conexao->prepare("INSERT INTO log_projeto (descricao_log, status_log) VALUES (?, ?)");
-    $stmt_logprojeto->bind_param("ss", $descricao, $nome_fantasia);
-    $stmt_logprojeto->execute(
-    // Obter o ID do último registro inserido
-    $fk_idlog_projeto = $conexao->insert_i
-    if ($fk_idlog_projeto === 0) {
-        die("Erro ao inserir registro na tabela 'log_projeto'.");
-   
+function insert_logprojeto($conexao, $cnpj, $nome_fantasia) {
+    $stmt_logprojeto = $conexao->prepare("INSERT INTO log_projeto (data_hora_criada,descricao_log,status_log,idusuario) VALUES (CURRENT_DATE(),?,'inativo',?)");
+    $stmt_logprojeto->bind_param("ss", $cnpj,$idusuario);
+    $stmt_logprojeto->execute();
+    $fk_idlog_projeto = $conexao->insert_id;
     $stmt_logprojeto->close();
-}
-function select_idusuario($conexao){
-    $stmt_idusuario = $conexao->prepare("SELECT idusuario,nome_usuario FROM usuario;")
-    $stmt_idusuario-> bind_param("ss",$id_db,$nome_db);
-    $stmt_idusuario-> execute();
-    $stmt_idusuario->close(
-    if($nome_db == $_SESSION['usuario']['nome']){
-        $fk_idusuario = $id_db
-    }else{
-        echo "Nome do banco não bate com nome da sessão;"
-    }
+    return $fk_idlog_projeto;
 }
 ?>
