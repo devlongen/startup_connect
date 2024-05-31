5a07fb538fec3f97ed2b74334f5f3c657d62c1aa
devlongen <iago.mendonca_2005@hotmail.com>
2024-05-31 09:01:21 -0300

    Funcionalida de alterar usuario, problema: id não está vindo

--- projeto/app/plugins_adm/app/php/salvarUsuario.php
+++ projeto/app/plugins_adm/app/php/salvarUsuario.php
@@ -3,15 +3,14 @@
 include('funcoes.php');
 
 
-// ERRO QUE NÃO ESTÁ RECEBENDO VALORES
+// NÃO ESTÁ RECEBENDO ID
 $idUsuario = $_POST["idusuario"] ?? null; // Inicializa como null caso não seja enviado
-$nome_usuario = $_POST["nome_usuario"] ?? "";
-$cpf_usuario  = $_POST["cpf_usuario"] ?? "";
-$telefone_usuario = $_POST["telefone_usuario"] ?? "";
-$data_nascimento = $_POST["data_nascimento"] ?? "";
-$email_usuario   = $_POST["email_usuario"] ?? "";
-$senha_usuario   = $_POST["senha_usuario"] ?? "";
-$status_usuario  = $_POST["status_usuario"] ?? "";
+$nome_usuario = $_POST["nome_alterar"] ?? "";
+$cpf_usuario  = $_POST["cpf_alterar"] ?? "";
+$telefone_usuario = $_POST["telefone_alterar"] ?? "";
+$data_nascimento = $_POST["data_nascimento_alterar"] ?? "";
+$email_usuario   = $_POST["email_alterar"] ?? "";
+$status_usuario  = $_POST["nAtivo_alterar"] ?? "";
 
 include("conexao.php");
 
@@ -20,17 +19,14 @@ if(isset($_GET['funcao'])) {
     $funcao = $_GET['funcao'];
     if ($funcao == "A") {
         // Monta a query SQL para atualização
-        $setSenha = "";
-        if (!empty($senha_usuario)) {
-            $setSenha = ", senha_usuario = '$senha_usuario'";
-        }
+        var_dump($idUsuario);
+        die();
         $sql = "UPDATE usuario SET "
              . "nome_usuario = '$nome_usuario', "
              . "cpf_usuario = '$cpf_usuario', "
              . "telefone_usuario = '$telefone_usuario', "
              . "data_nascimento_usuario = '$data_nascimento', "
              . "email_usuario = '$email_usuario' "
-             . "$setSenha "
              . "WHERE idusuario = '$idUsuario'";
     } elseif ($funcao == "D") {
         // Monta a query SQL para exclusão
