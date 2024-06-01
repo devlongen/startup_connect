657395634b7f92b91726ae4c1464a33b15366798
devlongen <iago.mendonca_2005@hotmail.com>
2024-06-01 10:41:47 -0300

    Insert funcionando 100% sem falhas algumas

--- projeto/app/plugins_empresa/dist/php/insert_empresa.php
+++ projeto/app/plugins_empresa/dist/php/insert_empresa.php
@@ -15,26 +15,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $descricao = $_POST['desc-startup'];
     $termo_condicao = $_POST['termos-condicao'];
 
-    $fk_idusuario = select_idusuario($conexao);
     $fk_idlog_projeto = insert_logprojeto($conexao, $cnpj);
     $fk_idtermo_condicao = insert_termo($conexao, $termo_condicao);
-
+    $fk_idusuario = select_idusuario($conexao);
     insert_empresa($conexao, $razao_social, $cnpj, $email, $nome_fantasia, $endereco, $data_abertura, $patrimonio, $meta_total, $descricao, $fk_idtermo_condicao, $fk_idusuario, $fk_idlog_projeto);
 }
 
 function select_idusuario($conexao) {
-    $stmt_idusuario = $conexao->prepare("SELECT idusuario FROM usuario LIMIT 1");
+    $stmt_idusuario = $conexao->prepare("SELECT idusuario FROM usuario WHERE nome_usuario = ?;");
+    $stmt_idusuario->bind_param("s", $_SESSION['usuario']['nome']);
     $stmt_idusuario->execute();
-    $stmt_idusuario->bind_result($idusuario);
+    $stmt_idusuario->bind_result($fk_idusuario);
     $stmt_idusuario->fetch();
     $stmt_idusuario->close();
 
-    return $idusuario;
+    return $fk_idusuario;
 }
 
 function insert_empresa($conexao, $razao_social, $cnpj, $email, $nome_fantasia, $endereco, $data_abertura, $patrimonio, $meta_total, $descricao, $fk_idtermo_condicao, $fk_idusuario, $fk_idlog_projeto) {
     $stmt_empresa = $conexao->prepare("INSERT INTO projeto (razao_social, cnpj_projeto, nome_fantasia, endereco, email_corporativo, data_abertura_empresa, data_abertura_site, patrimonio_oferecido, meta_total, desc_empresa, fk_idtermo_condicao, fk_idusuario, fk_idlog_projeto) VALUES (?, ?, ?, ?, ?, ?, CURRENT_DATE(), ?, ?, ?, ?, ?, ?)");
-    $stmt_empresa->bind_param("sssssssssssss", $razao_social, $cnpj, $nome_fantasia, $endereco, $email, $data_abertura, $patrimonio, $meta_total, $descricao, $fk_idtermo_condicao, $fk_idusuario, $fk_idlog_projeto);
+    $stmt_empresa->bind_param("ssssssddsiii", $razao_social, $cnpj, $nome_fantasia, $endereco, $email, $data_abertura, $patrimonio, $meta_total, $descricao, $fk_idtermo_condicao, $fk_idusuario, $fk_idlog_projeto);
     $stmt_empresa->execute();
     $stmt_empresa->close();
 }
@@ -49,10 +49,10 @@ function insert_termo($conexao, $termo_condicao) {
 }
 
 function insert_logprojeto($conexao, $cnpj) {
-    $stmt_logprojeto = $conexao->prepare("INSERT INTO log_projeto (data_hora_criada,descricao_log,status_log) VALUES (CURRENT_TIMESTAMP(),?,'inativo')");
+    $stmt_logprojeto = $conexao->prepare("INSERT INTO log_projeto (data_hora_criada, descricao_log, status_log) VALUES (CURRENT_TIMESTAMP(), ?, 'inativo')");
     $stmt_logprojeto->bind_param("s", $cnpj);
     $stmt_logprojeto->execute();
-    $fk_idlog_projeto = $conexao->insert_id;
+    $fk_idlog_projeto = $stmt_logprojeto->insert_id;
     $stmt_logprojeto->close();
     return $fk_idlog_projeto;
 }
