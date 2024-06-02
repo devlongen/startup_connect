5aa2418055d4c582d66c7711d8e434ca12b30f96
devlongen <iago.mendonca_2005@hotmail.com>
2024-06-01 22:31:38 -0300

    redirecionamento para investidor não poder criar startup

--- projeto/app/plugins_dashboard/dist/partes/sidebar.php
+++ projeto/app/plugins_dashboard/dist/partes/sidebar.php
@@ -7,62 +7,24 @@
 							<img src="../assets/img/profile.jpg" alt="..." class="avatar-img rounded-circle">
 						</div>
 						<div class="info">
-							<a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
+							<a data-toggle="collapse"aria-expanded="true">
 								<span>
 									<?php echo $_SESSION['usuario']['nome'] ?>
 									<span class="user-level"><?php echo $_SESSION['usuario']['status']?></span>
-									<span class="caret"></span>
 								</span>
 							</a>
 							<div class="clearfix"></div>
-
-							<div class="collapse in" id="collapseExample">
-								<ul class="nav">
-									<li>
-										<a href="#profile">
-											<span class="link-collapse">Meu perfil</span>
-										</a>
-									</li>
-									<li>
-										<a href="#edit">
-											<span class="link-collapse">Editar perfil</span>
-										</a>
-									</li>
-									<li>
-										<a href="#settings">
-											<span class="link-collapse">Configuração</span>
-										</a>
-									</li>
-								</ul>
-							</div>
 						</div>
 					</div>
 					
 					<ul class="nav nav-primary">
 						<li class="nav-item active">
-							<a  href="#dashboard" class="collapsed" aria-expanded="false">
+							<a  class="collapsed" aria-expanded="false">
 								<i class="fas fa-home"></i>
 								<p>Dashboard</p>
-								<span class="caret"></span>
 							</a>
 							
 						</li>
-						
-						
-						<li class="nav-item">
-							<a  href="usuarios.php">
-								<i class="fas fa-th-list"></i>
-								<p>Usuarios</p>
-								<span class="caret"></span>
-							</a>
-							
-						</li>
-						<li class="nav-item">
-							<a href="empresas.php">
-								<i class="fas fa-pen-square"></i>
-								<p>Empresas</p>
-								<span class="caret"></span>
-							</a>
 					</ul>
 				</div>
 			</div>
