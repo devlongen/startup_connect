8dd61be6f56b434a5e701698c56e4fab1816512a
alissonrvargas <alisson_r_vargas@estudante.sesisenai.org.br>
2024-06-11 21:51:13 -0300

    terminar o summernote e realizar testes com tabela projetos

--- projeto/app/plugins_empresa/dist/php/insert_empresa.php
+++ projeto/app/plugins_empresa/dist/php/insert_empresa.php
@@ -18,6 +18,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $meta_total = $_POST['meta-total'];
     $descricao = $_POST['desc-startup'];
     $termo_condicao = $_POST['termos-condicao'];
+    $summernote_introducao = $_POST[''];
+    $summernote_imagens = $_POST[''];
+    $summernote_objetivo = $_POST[''];
+    $summernote_video = $_POST[''];
 
     // Insere um registro na tabela log_projeto e obtém o ID gerado
     $fk_idlog_projeto = insert_logprojeto($conexao, $cnpj);
@@ -45,12 +49,18 @@ function select_idusuario($conexao) {
 }
 
 // Função para inserir um registro na tabela projeto (empresa)
-function insert_empresa($conexao, $razao_social, $cnpj, $email, $nome_fantasia, $endereco, $data_abertura, $patrimonio, $meta_total, $descricao, $fk_idtermo_condicao, $fk_idusuario, $fk_idlog_projeto) {
-    $stmt_empresa = $conexao->prepare("INSERT INTO projeto (razao_social, cnpj_projeto, nome_fantasia, endereco, email_corporativo, data_abertura_empresa, data_abertura_site, patrimonio_oferecido, meta_total, desc_empresa, fk_idtermo_condicao, fk_idusuario, fk_idlog_projeto) VALUES (?, ?, ?, ?, ?, ?, CURRENT_DATE(), ?, ?, ?, ?, ?, ?)");
-    $stmt_empresa->bind_param("ssssssddsiii", $razao_social, $cnpj, $nome_fantasia, $endereco, $email, $data_abertura, $patrimonio, $meta_total, $descricao, $fk_idtermo_condicao, $fk_idusuario, $fk_idlog_projeto);
-    $stmt_empresa->execute();
+function insert_empresa($conexao, $razao_social, $cnpj, $nome_fantasia, $endereco, $email, $data_abertura, $patrimonio, $meta_total, $descricao, $summernote_introducao, $summernote_imagens, $summernote_objetivo, $summernote_video, $fk_idtermo_condicao, $fk_idusuario, $fk_idlog_projeto) {
+    $stmt_empresa = $conexao->prepare("INSERT INTO projeto (razao_social, cnpj_projeto, nome_fantasia, endereco, email_corporativo, data_abertura_empresa, patrimonio_oferecido, meta_total, desc_empresa, summernote_introducao, summernote_imagens, summernote_objetivo, summernote_video, fk_idtermo_condicao, fk_idusuario, fk_idlog_projeto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    if (!$stmt_empresa) {
+        die('Erro na preparação da declaração SQL: ' . $conexao->error);
+    }
+    $stmt_empresa->bind_param("ssssssddssssiii", $razao_social, $cnpj, $nome_fantasia, $endereco, $email, $data_abertura, $patrimonio, $meta_total, $descricao, $summernote_introducao, $summernote_imagens, $summernote_objetivo, $summernote_video, $fk_idtermo_condicao, $fk_idusuario, $fk_idlog_projeto);
+    if (!$stmt_empresa->execute()) {
+        die('Erro na execução da declaração SQL: ' . $stmt_empresa->error);
+    }
     $stmt_empresa->close();
-    //header("Location: Redirecionar para alguma página pedindo para aguardar validação da administração")
+    // Redirecionamento para uma página de sucesso ou aguardo de validação
+    // header("Location: sua_pagina.php");
 }
 
 // Função para inserir um registro na tabela termo_condicao
