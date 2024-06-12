bb245be873b420f3ef5064875fd19684a732ecac
alissonrvargas <alisson_r_vargas@estudante.sesisenai.org.br>
2024-06-11 22:00:28 -0300

    tentando trocar a cor de fundo do adm

--- projeto/app/plugins_adm/app/partes/sidebar.php
+++ projeto/app/plugins_adm/app/partes/sidebar.php
@@ -1,5 +1,5 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-secondary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-2">
     <!-- Brand Logo -->
     <a href="../../" class="brand-link">
       <span class="brand-text font-weight-light">Startup Connect</span>
