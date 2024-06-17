3658a1f23891cb3f33fbd8ce7ac28ffeaa978c56
ElyandeAndrade <elyan.andrade3@gmail.com>
2024-06-17 19:21:35 -0300

    up

--- projeto/app/plugins_login/script.js
+++ projeto/app/plugins_login/script.js
@@ -34,10 +34,10 @@ function validacaoEmail(field) {
 function formatCPF(cpfInput) {
         let cpf = cpfInput.value;
         
-        // Remove any non-numeric characters
+        
         cpf = cpf.replace(/\D/g, "");
         
-        // Apply the CPF mask
+       
         if (cpf.length > 3 && cpf.length <= 6) {
             cpf = cpf.replace(/^(\d{3})(\d+)/, "$1.$2");
         } else if (cpf.length > 6 && cpf.length <= 9) {
@@ -46,7 +46,7 @@ function formatCPF(cpfInput) {
             cpf = cpf.replace(/^(\d{3})(\d{3})(\d{3})(\d+)/, "$1.$2.$3-$4");
         }
         
-        // Set the formatted value back to the input
+        
         cpfInput.value = cpf;
     }    
 
