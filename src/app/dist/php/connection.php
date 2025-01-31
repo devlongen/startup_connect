f8828b5fda240322d346fbc18b91fa626036214c
devlongen <iago.mendonca_2005@hotmail.com>
2025-01-31 09:14:31 -0300

    docker totalmente criado 100%

--- src/app/dist/php/connection.php
+++ src/app/dist/php/connection.php
@@ -2,7 +2,7 @@
 // Definição dos dados de conexão com o banco de dados
 $dbHost = 'localhost';        // Endereço do banco de dados
 $dbUser =  'root';             // Usuário do banco de dados
-$dbPassword = '';              // Senha do banco de dados
+$dbPassword = 'root';              // Senha do banco de dados
 $dbDatabase =  'startup_connect';  // Nome do banco de dados
 
 // Tentativa de conexão com o banco de dados
